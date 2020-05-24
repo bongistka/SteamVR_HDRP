2a121a185601bf3fe0fb870891b2deafe354404a
Barbora Darmova <darmova@gmail.com>
2020-05-24 23:48:04 +0200

    Laborant added safery tools added

---
+++ Assets/_Project/Scripts/RandomlyAnimate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomlyAnimate : MonoBehaviour
+{
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        float randomTime = Random.Range(5, 50);
+        Invoke("RandomThing", randomTime);
+    }
+
+    void RandomThing()
+    {
+        float randomTime = Random.Range(5, 150);
+
+        StartCoroutine("SetLaborantHappy");
+
+        Invoke("RandomThing", randomTime);
+    }
+
+    IEnumerator SetLaborantHappy()
+    {
+        animator.SetBool("IsHappy", true);
+        float randomTime = Random.Range(1, 20);
+        yield return new WaitForSeconds(randomTime);
+        Debug.Log("Laborant is randomly happy");
+        animator.SetBool("IsHappy", false);
+    }
+}
