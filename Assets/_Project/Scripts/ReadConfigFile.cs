23fc516500b5fa2860c92287bb3d936aa42671b5
Barbora Darmova <darmova@gmail.com>
2020-07-14 23:45:23 +0200

    Config bedheight

---
+++ Assets/_Project/Scripts/ReadConfigFile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ReadConfigFile : MonoBehaviour
+{
+    public MriSequence mriSequence;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReadTextFile(Application.dataPath + "/../Configs/" + "default.txt");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ReadTextFile(string file_path)
+    {
+        StreamReader inp_stm = new StreamReader(file_path, System.Text.Encoding.UTF8);
+
+        while (!inp_stm.EndOfStream)
+        {
+            string inp_ln = inp_stm.ReadLine();
+            if (inp_ln.Contains("#BedHeight: "))
+            {
+                float bedHeight = float.Parse(inp_ln.Replace("#BedHeight: ", ""));
+                if (bedHeight < 1.041462)
+                    mriSequence.bedStart = bedHeight;
+            }
+        }
+        inp_stm.Close();
+    }
+}
