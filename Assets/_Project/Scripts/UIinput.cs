2b3cc3a52edfbb738a0bba51fc36d233b09359cc
Barbora Darmova <darmova@gmail.com>
2020-07-19 11:12:51 +0200

    UI input control panel + ui controlled bed

--- Assets/_Project/Scripts/UIinput.cs
+++ Assets/_Project/Scripts/UIinput.cs
@@ -8,11 +8,16 @@ public class UIinput : MonoBehaviour
 {
     public InputField inputField;
     public MriSequence mriSequence;
+    public GameObject controlPanel;
+    public GameObject togglePanel;
+    public Button toggleButton;
+    private bool mainPanelVisible = false;
 
     // Start is called before the first frame update
     void Start()
     {
         inputField.placeholder.GetComponent<Text>().text = mriSequence.bedStart.ToString();
+        ToggleMainPanel();
     }
 
     // Update is called once per frame
@@ -23,7 +28,27 @@ public class UIinput : MonoBehaviour
 
     public void SetBedHeightFromInput(string input)
     {
-        float i = float.Parse(input.Replace(".", ","));
+        float i = float.Parse(input, CultureInfo.InvariantCulture);
         mriSequence.SetBedHeight(i);
+    }
+
+    public void ToggleMainPanel()
+    {
+        controlPanel.SetActive(mainPanelVisible);
+        if (!mainPanelVisible)
+        {
+            Vector3 pos = togglePanel.GetComponent<RectTransform>().anchoredPosition;
+            pos.x = -5;
+            togglePanel.GetComponent<RectTransform>().anchoredPosition = pos;
+            toggleButton.GetComponentInChildren<Text>().text = "<";
+        }
+        else
+        {
+            Vector3 pos = togglePanel.GetComponent<RectTransform>().anchoredPosition;
+            pos.x = -195;
+            togglePanel.GetComponent<RectTransform>().anchoredPosition = pos;
+            toggleButton.GetComponentInChildren<Text>().text = ">";
+        }
+        mainPanelVisible = !mainPanelVisible;
     }
 }
