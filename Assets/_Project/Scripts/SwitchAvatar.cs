d460a7d18d86e3709ae23f4d338931e17da442a2
Barbora Darmova <darmova@gmail.com>
2020-10-30 15:55:51 +0100

    Avatar female/male switch button

---
+++ Assets/_Project/Scripts/SwitchAvatar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchAvatar : MonoBehaviour
+{
+    [SerializeField] GameObject _femaleMesh;
+    [SerializeField] GameObject _maleMesh;
+    [SerializeField] Toggle _femaleToggle;
+    [SerializeField] Toggle _maleToggle;
+
+    private void Start()
+    {
+        SwitchFemaleMesh(_femaleToggle.isOn);
+        SwitchMaleMesh(_maleToggle.isOn);
+    }
+
+    public void SwitchFemaleMesh(bool isOn)
+    {
+        _femaleMesh.SetActive(isOn);
+        _maleMesh.SetActive(!isOn);
+        _maleToggle.isOn = !isOn;
+    }
+
+    public void SwitchMaleMesh(bool isOn)
+    {
+        _maleMesh.SetActive(isOn);
+        _femaleMesh.SetActive(!isOn);
+        _femaleToggle.isOn = !isOn;
+    }
+}
